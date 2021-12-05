c70e0b8c927b003ff3c7759a56f25677d1b40158
astherath <farceriv@gmail.com>
2021-12-05 15:09:38 -0500

    started day 2 challenge

--- src/day_1.rs
+++ src/day_1.rs
@@ -6,8 +6,20 @@ pub fn main() {
     let filename = "data.txt";
     // let filename = "test_data.txt";
     let data = parse(filename);
-    let resp = solve(data);
+    let resp = solve_sliding_window(data);
     dbg!(resp);
+}
+
+fn solve_sliding_window(data: Nums) -> usize {
+    let mut last: u32 = data[0..3].into_iter().sum();
+    data.windows(3)
+        .filter(|x| {
+            let window_sum = x.into_iter().sum();
+            let has_increased = window_sum > last;
+            last = window_sum;
+            has_increased
+        })
+        .count()
 }
 
 fn parse(filename: &str) -> Nums {
@@ -19,14 +31,13 @@ fn parse(filename: &str) -> Nums {
         .collect()
 }
 
-fn solve(data: Nums) -> u32 {
+fn _solve(data: Nums) -> usize {
     let mut last = data[0];
     data.into_iter()
         .filter(|x| {
             let has_increased = *x > last;
             last = *x;
-
             has_increased
         })
-        .count() as u32
+        .count()
 }
