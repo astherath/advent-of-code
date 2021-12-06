c6e0e21ef096c219005d04351e1e007d9eeec74e
astherath <farceriv@gmail.com>
2021-12-06 11:06:18 -0500

    started squid bingo

---
+++ src/day_4.rs
@@ -0,0 +1,9 @@
+#![allow(dead_code)]
+
+pub fn main() {}
+
+type ParsedData = Vec<String>;
+
+fn parse() -> ParsedData {
+    vec![]
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod day_1;
 mod day_2;
 mod day_3;
+mod day_4;
 
 fn main() {
     // day_1::main();
     // day_2::main();
-    day_3::main();
+    // day_3::main();
+    day_4::main();
 }
