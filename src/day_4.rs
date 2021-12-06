61d41a5efec9cf3d3697fdf6d7ef87fb2a59d5db
astherath <farceriv@gmail.com>
2021-12-06 12:47:21 -0500

    fixed from fn

--- src/day_4.rs
+++ src/day_4.rs
@@ -20,6 +20,7 @@ fn parse(filename: &str) -> ParsedData {
         .map(|x| x.parse::<u16>().unwrap())
         .collect::<Vec<u16>>();
 
+    const BOARD_SIZE: usize = 5;
     let boards = lines[1..]
         .into_iter()
         .fold(Vec::<Vec<String>>::new(), |mut acc, x| {
@@ -27,7 +28,7 @@ fn parse(filename: &str) -> ParsedData {
                 acc.insert(0, vec![x.to_string()]);
             } else {
                 let out_len = acc.len();
-                if acc[out_len - 1].len() < 5 {
+                if acc[out_len - 1].len() < BOARD_SIZE {
                     let inner_len = acc[out_len - 1].len();
                     acc[out_len - 1].insert(inner_len, x.to_string());
                 } else {
@@ -35,11 +36,14 @@ fn parse(filename: &str) -> ParsedData {
                 }
             }
             acc
-        });
+        })
+        .into_iter()
+        .map(|x| Board::from_matrix_string(x))
+        .collect();
 
     dbg!(&boards);
 
-    vec![]
+    (nums, boards)
 }
 
 fn solve(data: ParsedData) {}
@@ -50,8 +54,8 @@ impl Board {
             matrix: board
                 .into_iter()
                 .map(|x| {
-                    x.into_iter()
-                        .map(|x| (x.parse::<u16>().unwrap(), false))
+                    x.split_whitespace()
+                        .map(|x| (x.to_string().parse::<u16>().unwrap(), false))
                         .collect::<Vec<Num>>()
                 })
                 .collect::<BoardMatrix>(),
@@ -70,8 +74,8 @@ impl Board {
     }
 }
 
-type ParsedData = Vec<String>;
-type MatrixString = Vec<Vec<String>>;
+type ParsedData = (Vec<u16>, Vec<Board>);
+type MatrixString = Vec<String>;
 type Num = (u16, bool);
 type BoardMatrix = Vec<Vec<Num>>;
 
